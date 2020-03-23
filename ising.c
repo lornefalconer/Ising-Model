8d6418b02d280b7319ce6728f4d6b648ce8e6c8b
lornefalconer <60292559+lornefalconer@users.noreply.github.com>
2020-03-23 22:23:13 +0000

    Add files via upload

--- ising.c
+++ ising.c
@@ -16,17 +16,17 @@
 #define T_0 (J/K_B)
 
 #define ITERATIONS 100000
+#define TEMP_START 1.0
 #define TEMP_MAX (5*T_0)
 #define TEMP_INTERVAL (TEMP_MAX/500)
 #define MAXLENGTH 256
-#define D 5/*100 seems the upper limit to reasonable processing time for serial computing.*/
+#define D 20/*100 seems the upper limit to reasonable processing time for serial computing.*/
 #define UP 1
 #define DOWN -1
 #define S(x,y) spin_array[D*((D+(x))%D) + ((D+(y))%D)]
 #define STDEV_ARRAY_LENGTH 1000
 
-
-
+#define INVESTIGATE_C_VS_D YES
 #define CUTOFF_TESTING YES
 
 #define MEM_ERROR 3
@@ -37,6 +37,7 @@
 #define SCRIPT_EN "total_E_gnuplot.script"
 #define SCRIPT_C "specific_heat_with_temp_plot.script"
 #define SCRIPT_M "gnuplot_magnetism.script"
+#define SCRIPT_S "gnuplot_susceptability.script"
 
 /*RNG selection macros taken from mat_gen.c by CDHW.*/
 #define FAIL 0.0
@@ -50,7 +51,7 @@
 #define PLOTTING YES
 #define PLOTTING_DEBUG NO
 
-#define RANDOM_SPIN NO
+#define RANDOM_SPIN YES
 
 #define SERIAL YES
 #define PARALLEL NO
@@ -183,7 +184,18 @@ static void write_specific_heat_to_file(double* specific_heat_array, double* tem
     fprintf(fc,"#C\tTemp\n");
 
     for(int i = 0; i<(TEMP_MAX/TEMP_INTERVAL); i++){
-            fprintf(fc,"%lg\t%lg\n",temp_array[i]/T_0,specific_heat_array[i]/(D*D*K_B));
+            fprintf(fc,"%lg\t%lg\n",T_0*fabs(temp_array[i]/T_0 - 2.27),specific_heat_array[i]/(D*D*K_B));
+        }
+    fclose(fc);
+}
+
+static void write_susceptability_to_file(double* susceptability, double* temp_array){
+    FILE *fc = fopen("susceptability.txt", "w+");/*opens file to write the ith body data to*/
+    err_check_file(fc);
+    fprintf(fc,"#Chi\tTemp\n");
+
+    for(int i = 0; i<(TEMP_MAX/TEMP_INTERVAL); i++){
+            fprintf(fc,"%lg\t%lg\n",T_0*fabs(temp_array[i]/T_0 - 2.27),susceptability[i]/(D*D*K_B));
         }
     fclose(fc);
 }
@@ -194,7 +206,7 @@ static void write_magnetism_to_file(double* magnetism_array, double* temp_array)
     fprintf(fc,"#M\tTemp\n");
 
     for(int i = 0; i<(TEMP_MAX/TEMP_INTERVAL); i++){
-            fprintf(fc,"%lg\t%lg\n",temp_array[i]/T_0,magnetism_array[i]/(MU0*D*D));
+            fprintf(fc,"%lg\t%lg\n",T_0*fabs(temp_array[i]/T_0 - 2.27),magnetism_array[i]/(MU0*D*D));
         }
     fclose(fc);
 }
@@ -243,22 +255,22 @@ the previous average. If they differ, the system is not in equilibrium and the o
 If they are the same (within a suitable uncertainty window oweing to floating point arithmetic) the return value of the function is
 modified to break the outer loop.*/
 
-static double store_consecutive_total_energy_calc_average(double* array_of_consecutive_total_energy, double total_energy, int iteration){
+static double store_consecutive_quantity_calc_average(double* array_of_consecutive_quantity, double quantity, int iteration){
 
     int modulo_element = (STDEV_ARRAY_LENGTH+iteration)%STDEV_ARRAY_LENGTH;
 
     //printf("%d\n",modulo_element);
 
-    array_of_consecutive_total_energy[modulo_element] = total_energy;
+    array_of_consecutive_quantity[modulo_element] = quantity;
 
     if(STDEV_ARRAY_LENGTH-1 == modulo_element){/*If the array has filled up*/
-       double total_energy_sum = 0.0;/*Initializes total_energy to floating point 0.0*/
+       double total_quantity_sum = 0.0;/*Initializes total_energy to floating point 0.0*/
        for(int i = 0; i<STDEV_ARRAY_LENGTH;i++){
-            total_energy_sum += array_of_consecutive_total_energy[i];
+            total_quantity_sum += array_of_consecutive_quantity[i];
        }
-       double total_energy_av = total_energy_sum/STDEV_ARRAY_LENGTH;/*store average value*/
+       double total_quantity_av = total_quantity_sum/STDEV_ARRAY_LENGTH;/*store average value*/
        //printf("test total energy ac %g\n",total_energy_av);
-       return total_energy_av;
+       return total_quantity_av;
     }
 
 
@@ -270,14 +282,15 @@ static double store_consecutive_total_energy_calc_average(double* array_of_conse
 
 
 static double calculate_magnetisation(int* spin_array){
-    double total_spin = 0.0;
+    int total_spin = 0;
     for(int i = 0; i< D*D; i++){
         total_spin += spin_array[i];
 
     }
-    printf("TEST: TOTAL SPIN: %f\n",total_spin);
-    double Magnetisation = total_spin/(D*D);
-    printf("MAGE TEST: %lf\n",Magnetisation);
+
+    //printf("TEST: TOTAL SPIN: %d\n",total_spin);
+    double Magnetisation = fabs((double)total_spin/(D*D));
+    //printf("MAGE TEST: %lf\n",Magnetisation);
 
 
     return Magnetisation;
@@ -307,6 +320,22 @@ static double calculate_specific_heat(double* energy_squared_array, double* arra
 
 }
 
+static double calculate_susceptability(double* magnetism_squared_array, double* array_of_consecutive_magnetism, double average_magnetism,double T){
+    double total_magnetism_squared = 0.0;
+    for(int j = 0; j<STDEV_ARRAY_LENGTH;j++){
+                    magnetism_squared_array[j] = array_of_consecutive_magnetism[j]*array_of_consecutive_magnetism[j];
+                    total_magnetism_squared += magnetism_squared_array[j];
+
+            }
+    double average_magnetism_squared = total_magnetism_squared/STDEV_ARRAY_LENGTH;
+    double susceptability = (average_magnetism_squared - pow(average_magnetism,2))/(K_B*T);
+    printf("susceptability: %.25e\n",susceptability);
+    return susceptability;
+
+}
+
+
+
 ///Need to fiddle with this so it re-initializes the spin array before the next temperature iteration///
 
 static double* evolve_system_random_spin_beta(int* spin_array, VAR_TYPE rng_x_coord, VAR_TYPE rng_y_coord, VAR_TYPE rng_Metropolis_U, double Temp){
@@ -315,9 +344,12 @@ static double* evolve_system_random_spin_beta(int* spin_array, VAR_TYPE rng_x_co
     double *total_energy_array = malloc(ITERATIONS*sizeof(double));
     double *array_of_consecutive_total_E = malloc(STDEV_ARRAY_LENGTH*sizeof(double));/*Array to store consecutive calculated total_E values.*/
     double *energy_squared_array = malloc(STDEV_ARRAY_LENGTH*sizeof(double));
+    double *magnetism_squared_array = malloc(STDEV_ARRAY_LENGTH*sizeof(double));
+    double *array_of_consecutive_magnetism = malloc(STDEV_ARRAY_LENGTH*sizeof(double));
 
     double average_total_energy_1 = 0.0;
     double average_total_energy_2 = NAN;
+    double average_total_magnetism = 0.0;
     double relative_error = 100.0;
     int count = 0;
 
@@ -330,26 +362,33 @@ static double* evolve_system_random_spin_beta(int* spin_array, VAR_TYPE rng_x_co
             double total_energy = evaluate_total_energy(spin_array);/*evaluate total energy of spin array*/
             total_energy_array[i] = total_energy;/*Store the energy in an array for later plotting and eqm analysis.*/
 
+
+
+
+            double magnetism = calculate_magnetisation(spin_array);
+
             if(CUTOFF_TESTING){
-            double result = store_consecutive_total_energy_calc_average(array_of_consecutive_total_E,total_energy,i);
-            if(result != 0){
+                double result_E = store_consecutive_quantity_calc_average(array_of_consecutive_total_E,total_energy,i);
+                double result_M = store_consecutive_quantity_calc_average(array_of_consecutive_magnetism,magnetism,i);
+                if(result_E != 0){
                     //printf("result %g\n",result);
-            }
-            if((result != FAIL) && (count == 0)){
-                average_total_energy_1 = result;
-                count = 1;
-            }
-            else if(result!= FAIL){
-                average_total_energy_2 = result;
-                count = 0;
+                }
+                if((result_E != FAIL) && (count == 0)){
+                    average_total_energy_1 = result_E;
+                    count = 1;
+                }
+                else if(result_E!= FAIL){
+                    average_total_energy_2 = result_E;
+                    average_total_magnetism = result_M;
+                    count = 0;
                 //printf("average_1 is %g average_2 is %g\n", average_total_energy_1,average_total_energy_2);
 
-            }
+                }
 
-            if(fabs((average_total_energy_1 - average_total_energy_2)/average_total_energy_2) < 0.0001){
+                if(fabs((average_total_energy_1 - average_total_energy_2)/average_total_energy_2) < 0.0001){
 
-                break;
-            }
+                    break;
+                }
             }
 
             if( RANDOM_SPIN ){
@@ -394,12 +433,14 @@ static double* evolve_system_random_spin_beta(int* spin_array, VAR_TYPE rng_x_co
         }
 
     double specific_heat = calculate_specific_heat(energy_squared_array, array_of_consecutive_total_E, average_total_energy_2,Temp);
+    double susceptability = calculate_susceptability(magnetism_squared_array,array_of_consecutive_magnetism,average_total_magnetism,Temp);
     double magnetism = calculate_magnetisation(spin_array);
 
-    double *ret_vals = (double*)malloc(2*sizeof(double));
+    double *ret_vals = (double*)malloc(3*sizeof(double));
     ret_vals[0] = specific_heat;
     ret_vals[1] = magnetism;
-    printf("MAGNETSATION: %.15g\n", calculate_magnetisation(spin_array));
+    ret_vals[2] = susceptability;
+    //printf("MAGNETSATION: %.15g\n", calculate_magnetisation(spin_array));
 
     spin_array_write_to_file(spin_array);
     write_total_E_to_file(total_energy_array);
@@ -431,18 +472,10 @@ Magnetisation is the sum of spins over the number of spins.
 **/
 
 
-/*Plots spin array. Currently hardcoded into random initializastion.
-Next step is to make this an independent function, callable at any point.*/
 
-int main()
-{
+static double repeat_with_iterating_temp(){
 
-if(SERIAL){/**EXECUTES IN ABOUT 22s FOR 100000 ITERATIONS**/
-
-    int rounds = 5;
-    int bound = 2;
-    int i = 0;
-    VAR_TYPE rng_initialize_spin_array;
+    VAR_TYPE rng_initialize_spin_array;/*declare variable to initialize pcg rng*/
     SRANDOM_R(&rng_initialize_spin_array, SEED, (intptr_t)&rng_initialize_spin_array);/*Seeds RND, Make sure RNG is seeded first.*/
 
     printf("%d\n",rng_initialize_spin_array);
@@ -456,7 +489,7 @@ if(SERIAL){/**EXECUTES IN ABOUT 22s FOR 100000 ITERATIONS**/
 
 
 
-    spin_array_write_to_file(spin_array);
+    spin_array_write_to_file(spin_array);/*writes the initial spin configuration to a file for debug plotting.*/
 
     //if( PLOTTING_DEBUG ){
       //  plot(GNUPLOT,SCRIPT_SPIN);
@@ -475,33 +508,51 @@ if(SERIAL){/**EXECUTES IN ABOUT 22s FOR 100000 ITERATIONS**/
     }
 */
 
-    VAR_TYPE rng_x_coord, rng_y_coord, rng_Metropolis_U;
+    VAR_TYPE rng_x_coord, rng_y_coord, rng_Metropolis_U;/*decalre variables to initialize 3 other rngs*/
     SRANDOM_R(&rng_x_coord, SEED, (intptr_t)&rng_x_coord);/*Seeds RNG stream for x_coord of spin.*/
     SRANDOM_R(&rng_y_coord, SEED, (intptr_t)&rng_y_coord);/*Seeds RNG stream for y coord of spin.*/
     SRANDOM_R(&rng_Metropolis_U, SEED, (intptr_t)&rng_Metropolis_U);/*Seeds RNG for Metropolis rn.*/
 
 
-    double Temp = 1.0;
+    double Temp = TEMP_START;/*Declare starting temperature for interation.*/
 
-
+    /*allocate memory for plotting of C,M and Chi*/
     double* specific_heat_array = malloc((TEMP_MAX/TEMP_INTERVAL)*sizeof(double));
     double* magnetism_array = malloc((TEMP_MAX/TEMP_INTERVAL)*sizeof(double));
+    double* susceptability_array = malloc((TEMP_MAX/TEMP_INTERVAL)*sizeof(double));
+
+    /*Allocate memory for plotting of temperature againt above quantities*/
     double* temp_array = malloc((TEMP_MAX/TEMP_INTERVAL)*sizeof(double));
+    /*Count variable will ensure allocation of memory to correct indices in memory*/
     int count = 0;
 
+    double max_val = 0.0;
 
-    while(Temp < TEMP_MAX){
+    while(Temp < TEMP_MAX){/*While the temp is less tan some upper limit*/
         printf("\n\nTEMP:%f\n\n",Temp);
 
         initialize_spin_array(spin_array);/*Initialize spin array to random spins.*/
-                if(PLOTTING_DEBUG){
+                if(PLOTTING_DEBUG && Temp == 1.0){
                     spin_array_write_to_file(spin_array);/*Write to file in case you want to debug the plot*/
                 }
         /*Evolvoe the system with time and the current Temperature*/
 
         double *ret_vals = evolve_system_random_spin_beta(spin_array, rng_x_coord, rng_y_coord, rng_Metropolis_U, Temp);
         double specific_heat = ret_vals[0];
+
+        if(INVESTIGATE_C_VS_D){/*If you are investigating the relationship between C and D*/
+            if(Temp == TEMP_START){
+                max_val = specific_heat;
+
+            }
+            else if(specific_heat > max_val){
+                    max_val = specific_heat;
+
+            }
+        }
+
         double magnetism = ret_vals[1];
+        double susceptability = ret_vals[2];
         free(ret_vals);
 
 
@@ -512,6 +563,7 @@ if(SERIAL){/**EXECUTES IN ABOUT 22s FOR 100000 ITERATIONS**/
 
         specific_heat_array[count] = specific_heat;
         magnetism_array[count] = magnetism;
+        susceptability_array[count] = susceptability;
         temp_array[count] = Temp;
 
 
@@ -522,9 +574,12 @@ if(SERIAL){/**EXECUTES IN ABOUT 22s FOR 100000 ITERATIONS**/
         printf("MAGNETSATION: %f\n", magnetism);
 
     }
+    printf("MAX C %.15e\n",max_val);
     write_specific_heat_to_file(specific_heat_array, temp_array);
     write_magnetism_to_file(magnetism_array,temp_array);
+    write_susceptability_to_file(susceptability_array,temp_array);
 
+    free(susceptability_array);
     free(specific_heat_array);
     free(magnetism_array);
     free(temp_array);
@@ -535,6 +590,139 @@ if(SERIAL){/**EXECUTES IN ABOUT 22s FOR 100000 ITERATIONS**/
         //plot(GNUPLOT,SCRIPT_SPIN);
         plot(GNUPLOT,SCRIPT_C);
         plot(GNUPLOT,SCRIPT_M);
+        plot(GNUPLOT,SCRIPT_S);
+
+    }
+}
+
+
+/*Plots spin array. Currently hardcoded into random initializastion.
+Next step is to make this an independent function, callable at any point.*/
+
+int main()
+{
+
+if(SERIAL){/**EXECUTES IN ABOUT 22s FOR 100000 ITERATIONS**/
+
+    int rounds = 5;
+    int bound = 2;
+    int i = 0;
+    VAR_TYPE rng_initialize_spin_array;/*declare variable to initialize pcg rng*/
+    SRANDOM_R(&rng_initialize_spin_array, SEED, (intptr_t)&rng_initialize_spin_array);/*Seeds RND, Make sure RNG is seeded first.*/
+
+    printf("%d\n",rng_initialize_spin_array);
+
+
+
+    int *spin_array = malloc(D*D*sizeof(int));/*Allocates memory for spin array.*/
+
+    initialize_spin_array(spin_array);/*Initializes spin array to random arrangement of +/-1.*/
+
+
+
+
+    spin_array_write_to_file(spin_array);/*writes the initial spin configuration to a file for debug plotting.*/
+
+    //if( PLOTTING_DEBUG ){
+      //  plot(GNUPLOT,SCRIPT_SPIN);
+    //}
+
+
+
+    printf("MAGNETSATION: %.25e\n", calculate_magnetisation(spin_array));
+
+/*
+    for(int x = 0; x < D ; x++){
+        for(int y = 0; y < D; y++){
+            double delta_E = calculate_delta_E(spin_array,x,y);
+            printf("Flipping Energy is: %.15lg\n",delta_E);
+        }
+    }
+*/
+
+    VAR_TYPE rng_x_coord, rng_y_coord, rng_Metropolis_U;/*decalre variables to initialize 3 other rngs*/
+    SRANDOM_R(&rng_x_coord, SEED, (intptr_t)&rng_x_coord);/*Seeds RNG stream for x_coord of spin.*/
+    SRANDOM_R(&rng_y_coord, SEED, (intptr_t)&rng_y_coord);/*Seeds RNG stream for y coord of spin.*/
+    SRANDOM_R(&rng_Metropolis_U, SEED, (intptr_t)&rng_Metropolis_U);/*Seeds RNG for Metropolis rn.*/
+
+
+    double Temp = TEMP_START;/*Declare starting temperature for interation.*/
+
+    /*allocate memory for plotting of C,M and Chi*/
+    double* specific_heat_array = malloc((TEMP_MAX/TEMP_INTERVAL)*sizeof(double));
+    double* magnetism_array = malloc((TEMP_MAX/TEMP_INTERVAL)*sizeof(double));
+    double* susceptability_array = malloc((TEMP_MAX/TEMP_INTERVAL)*sizeof(double));
+
+    /*Allocate memory for plotting of temperature againt above quantities*/
+    double* temp_array = malloc((TEMP_MAX/TEMP_INTERVAL)*sizeof(double));
+    /*Count variable will ensure allocation of memory to correct indices in memory*/
+    int count = 0;
+
+    double max_val = 0.0;
+
+    while(Temp < TEMP_MAX){/*While the temp is less tan some upper limit*/
+        printf("\n\nTEMP:%f\n\n",Temp);
+
+        initialize_spin_array(spin_array);/*Initialize spin array to random spins.*/
+                if(PLOTTING_DEBUG && Temp == 1.0){
+                    spin_array_write_to_file(spin_array);/*Write to file in case you want to debug the plot*/
+                }
+        /*Evolvoe the system with time and the current Temperature*/
+
+        double *ret_vals = evolve_system_random_spin_beta(spin_array, rng_x_coord, rng_y_coord, rng_Metropolis_U, Temp);
+        double specific_heat = ret_vals[0];
+
+        if(INVESTIGATE_C_VS_D){/*If you are investigating the relationship between C and D*/
+            if(Temp == TEMP_START){
+                max_val = specific_heat;
+
+            }
+            else if(specific_heat > max_val){
+                    max_val = specific_heat;
+
+            }
+        }
+
+        double magnetism = ret_vals[1];
+        double susceptability = ret_vals[2];
+        free(ret_vals);
+
+
+                if(PLOTTING_DEBUG){
+                        spin_array_write_to_file(spin_array);
+                        plot(GNUPLOT,SCRIPT_SPIN);
+                }
+
+        specific_heat_array[count] = specific_heat;
+        magnetism_array[count] = magnetism;
+        susceptability_array[count] = susceptability;
+        temp_array[count] = Temp;
+
+
+        Temp += TEMP_INTERVAL;
+        count += 1;
+
+        printf("Specific heat: %.25e\n", specific_heat);
+        printf("MAGNETSATION: %f\n", magnetism);
+
+    }
+    printf("MAX C %.15e\n",max_val);
+    write_specific_heat_to_file(specific_heat_array, temp_array);
+    write_magnetism_to_file(magnetism_array,temp_array);
+    write_susceptability_to_file(susceptability_array,temp_array);
+
+    free(susceptability_array);
+    free(specific_heat_array);
+    free(magnetism_array);
+    free(temp_array);
+    free(spin_array);
+
+
+    if(PLOTTING){
+        //plot(GNUPLOT,SCRIPT_SPIN);
+        plot(GNUPLOT,SCRIPT_C);
+        plot(GNUPLOT,SCRIPT_M);
+        plot(GNUPLOT,SCRIPT_S);
 
     }
 
